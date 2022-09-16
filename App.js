c93974f77dbbe3f7f9e9d79a12c6998f5a0efea8
RCONDORI26 <rcmuruchi26@gmail.com>
2022-09-16 15:29:57 -0400

    Sincronizar mi Codigo Local con Mi GitHub

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+const App = () => {
+  return <Text> Hola Mundo, Aprendiendo de una Todo</Text>
+};
+
+export default App;
---
+++ src/screens/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component} from 'react' ;
+import { View, Text } from 'react-native';
+
+class Login extends Component {
+
+    render(){
+        return(
+             <View>
+               <Text>
+                 Mi primer Codigo en React
+               </Text>
+             </View>
+        )
+    }
+}
+
+export default Login
